98fc01c2cc8f34927871740a6d119a1827bc8d96
zqm64 <zqm64@outlook.com>
2021-04-03 13:02:51 +0800

    mi8937: Import IR blobs

---
+++ mi8937-vendor-ir.mk
@@ -0,0 +1,6 @@
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi/mi8937/ir/vendor/lib64/hw/ir-led/consumerir.default.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/ir-led/consumerir.default.so \
+    vendor/xiaomi/mi8937/ir/vendor/lib64/hw/ir-pwm/consumerir.default.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/ir-pwm/consumerir.default.so \
+    vendor/xiaomi/mi8937/ir/vendor/lib64/hw/ir-spidev/consumerir.default.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/ir-spidev/consumerir.default.so \
+    vendor/xiaomi/mi8937/ir/vendor/lib/hw/ir-led/consumerir.default.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/ir-led/consumerir.default.so \
+    vendor/xiaomi/mi8937/ir/vendor/lib/hw/ir-spidev/consumerir.default.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/ir-spidev/consumerir.default.so
--- mi8937-vendor.mk
+++ mi8937-vendor.mk
@@ -7,3 +7,4 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Inherit fragments
 $(call inherit-product, vendor/xiaomi/mi8937/mi8937-vendor-acdb.mk)
+$(call inherit-product, vendor/xiaomi/mi8937/mi8937-vendor-ir.mk)
