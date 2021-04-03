e149e86042ac27095727cd04f595e945094bba93
zqm64 <zqm64@outlook.com>
2021-04-03 22:44:33 +0800

    mi8937: fingerprint: Import ugg blobs

---
+++ mi8937-vendor-fingerprint.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi/mi8937/fingerprint/vendor/firmware/goodix_firmware.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/goodix_firmware.bin \
+    vendor/xiaomi/mi8937/fingerprint/vendor/lib64/hw/fingerprint.ugg_fpc.default.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/fingerprint.ugg_fpc.default.so \
+    vendor/xiaomi/mi8937/fingerprint/vendor/lib64/hw/gf_fingerprint.default.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/gf_fingerprint.default.so \
+    vendor/xiaomi/mi8937/fingerprint/vendor/lib64/lib_fpc_tac_shared.so:$(TARGET_COPY_OUT_VENDOR)/lib64/lib_fpc_tac_shared.so \
+    vendor/xiaomi/mi8937/fingerprint/vendor/lib64/libgf_algo.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libgf_algo.so \
+    vendor/xiaomi/mi8937/fingerprint/vendor/lib64/libgf_ca.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libgf_ca.so \
+    vendor/xiaomi/mi8937/fingerprint/vendor/lib64/libgf_hal.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libgf_hal.so \
+    vendor/xiaomi/mi8937/fingerprint/vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so \
+    vendor/xiaomi/mi8937/fingerprint/vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libvendor.goodix.hardware.fingerprint@1.0.so \
+    vendor/xiaomi/mi8937/fingerprint/vendor/lib/lib_fpc_tac_shared.so:$(TARGET_COPY_OUT_VENDOR)/lib/lib_fpc_tac_shared.so
