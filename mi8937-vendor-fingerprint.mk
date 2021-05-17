97f8129f8421ab65fa38a9ae6eec2a2fd1a343b7
zqm64 <zqm64@outlook.com>
2021-05-17 10:10:56 +0800

    mi8937: fp: Import old goodix blobs from jabiyeff santoni

--- mi8937-vendor-fingerprint.mk
+++ mi8937-vendor-fingerprint.mk
@@ -1,13 +1,22 @@
 PRODUCT_COPY_FILES += \
+    vendor/xiaomi/mi8937/fingerprint/vendor/bin/gx_fpcmd:$(TARGET_COPY_OUT_VENDOR)/bin/gx_fpcmd \
+    vendor/xiaomi/mi8937/fingerprint/vendor/bin/gx_fpd:$(TARGET_COPY_OUT_VENDOR)/bin/gx_fpd \
     vendor/xiaomi/mi8937/fingerprint/vendor/firmware/goodix_firmware.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/goodix_firmware.bin \
+    vendor/xiaomi/mi8937/fingerprint/vendor/lib64/hw/fingerprint.goodix.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/fingerprint.goodix.so \
     vendor/xiaomi/mi8937/fingerprint/vendor/lib64/hw/fingerprint.land_fpc.default.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/fingerprint.land_fpc.default.so \
     vendor/xiaomi/mi8937/fingerprint/vendor/lib64/hw/fingerprint.ugg_fpc.default.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/fingerprint.ugg_fpc.default.so \
     vendor/xiaomi/mi8937/fingerprint/vendor/lib64/hw/gf_fingerprint.default.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/gf_fingerprint.default.so \
+    vendor/xiaomi/mi8937/fingerprint/vendor/lib64/hw/gxfingerprint.default.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/gxfingerprint.default.so \
+    vendor/xiaomi/mi8937/fingerprint/vendor/lib64/libcom_fingerprints_service.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libcom_fingerprints_service.so \
+    vendor/xiaomi/mi8937/fingerprint/vendor/lib64/libfp_client.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libfp_client.so \
     vendor/xiaomi/mi8937/fingerprint/vendor/lib64/lib_fpc_tac_shared.so:$(TARGET_COPY_OUT_VENDOR)/lib64/lib_fpc_tac_shared.so \
+    vendor/xiaomi/mi8937/fingerprint/vendor/lib64/libfpservice.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libfpservice.so \
     vendor/xiaomi/mi8937/fingerprint/vendor/lib64/libgf_algo.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libgf_algo.so \
     vendor/xiaomi/mi8937/fingerprint/vendor/lib64/libgf_ca.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libgf_ca.so \
     vendor/xiaomi/mi8937/fingerprint/vendor/lib64/libgf_hal.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libgf_hal.so \
+    vendor/xiaomi/mi8937/fingerprint/vendor/lib64/libgxbinder.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libgxbinder.so \
     vendor/xiaomi/mi8937/fingerprint/vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so \
     vendor/xiaomi/mi8937/fingerprint/vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libvendor.goodix.hardware.fingerprint@1.0.so \
     vendor/xiaomi/mi8937/fingerprint/vendor/lib/hw/fingerprint.land_fpc.default.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/fingerprint.land_fpc.default.so \
+    vendor/xiaomi/mi8937/fingerprint/vendor/lib/libcom_fingerprints_service.so:$(TARGET_COPY_OUT_VENDOR)/lib/libcom_fingerprints_service.so \
     vendor/xiaomi/mi8937/fingerprint/vendor/lib/lib_fpc_tac_shared.so:$(TARGET_COPY_OUT_VENDOR)/lib/lib_fpc_tac_shared.so
