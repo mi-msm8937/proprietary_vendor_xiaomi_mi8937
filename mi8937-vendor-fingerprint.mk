fc1fbe184bc4e7cf9a22a787a2b0408232208574
zqm64 <zqm64@outlook.com>
2021-05-17 10:05:01 +0800

    mi8937: fingerprint: Import land_fpc

--- mi8937-vendor-fingerprint.mk
+++ mi8937-vendor-fingerprint.mk
@@ -1,5 +1,6 @@
 PRODUCT_COPY_FILES += \
     vendor/xiaomi/mi8937/fingerprint/vendor/firmware/goodix_firmware.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/goodix_firmware.bin \
+    vendor/xiaomi/mi8937/fingerprint/vendor/lib64/hw/fingerprint.land_fpc.default.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/fingerprint.land_fpc.default.so \
     vendor/xiaomi/mi8937/fingerprint/vendor/lib64/hw/fingerprint.ugg_fpc.default.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/fingerprint.ugg_fpc.default.so \
     vendor/xiaomi/mi8937/fingerprint/vendor/lib64/hw/gf_fingerprint.default.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/gf_fingerprint.default.so \
     vendor/xiaomi/mi8937/fingerprint/vendor/lib64/lib_fpc_tac_shared.so:$(TARGET_COPY_OUT_VENDOR)/lib64/lib_fpc_tac_shared.so \
@@ -8,4 +9,5 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/mi8937/fingerprint/vendor/lib64/libgf_hal.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libgf_hal.so \
     vendor/xiaomi/mi8937/fingerprint/vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so \
     vendor/xiaomi/mi8937/fingerprint/vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libvendor.goodix.hardware.fingerprint@1.0.so \
+    vendor/xiaomi/mi8937/fingerprint/vendor/lib/hw/fingerprint.land_fpc.default.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/fingerprint.land_fpc.default.so \
     vendor/xiaomi/mi8937/fingerprint/vendor/lib/lib_fpc_tac_shared.so:$(TARGET_COPY_OUT_VENDOR)/lib/lib_fpc_tac_shared.so
