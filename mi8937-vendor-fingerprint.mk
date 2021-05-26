0ae304f74f36d33f0ddb7fd83943ecf0f850afd7
zqm64 <zqm64@outlook.com>
2021-05-26 20:39:32 +0800

    mi8937: fp: Import santoni_fpc

--- mi8937-vendor-fingerprint.mk
+++ mi8937-vendor-fingerprint.mk
@@ -3,6 +3,7 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/mi8937/fingerprint/vendor/bin/santoni_gx_fpd:$(TARGET_COPY_OUT_VENDOR)/bin/santoni_gx_fpd \
     vendor/xiaomi/mi8937/fingerprint/vendor/firmware/goodix_firmware.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/goodix_firmware.bin \
     vendor/xiaomi/mi8937/fingerprint/vendor/lib64/hw/fingerprint.land_fpc.default.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/fingerprint.land_fpc.default.so \
+    vendor/xiaomi/mi8937/fingerprint/vendor/lib64/hw/fingerprint.santoni_fpc.default.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/fingerprint.santoni_fpc.default.so \
     vendor/xiaomi/mi8937/fingerprint/vendor/lib64/hw/fingerprint.santoni.goodix.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/fingerprint.santoni.goodix.so \
     vendor/xiaomi/mi8937/fingerprint/vendor/lib64/hw/fingerprint.ugg_fpc.default.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/fingerprint.ugg_fpc.default.so \
     vendor/xiaomi/mi8937/fingerprint/vendor/lib64/hw/gf_fingerprint.default.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/gf_fingerprint.default.so \
@@ -18,5 +19,6 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/mi8937/fingerprint/vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so \
     vendor/xiaomi/mi8937/fingerprint/vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libvendor.goodix.hardware.fingerprint@1.0.so \
     vendor/xiaomi/mi8937/fingerprint/vendor/lib/hw/fingerprint.land_fpc.default.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/fingerprint.land_fpc.default.so \
+    vendor/xiaomi/mi8937/fingerprint/vendor/lib/hw/fingerprint.santoni_fpc.default.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/fingerprint.santoni_fpc.default.so \
     vendor/xiaomi/mi8937/fingerprint/vendor/lib/lib_fpc_tac_shared.so:$(TARGET_COPY_OUT_VENDOR)/lib/lib_fpc_tac_shared.so \
     vendor/xiaomi/mi8937/fingerprint/vendor/lib/libSFm_fingerprints_service.so:$(TARGET_COPY_OUT_VENDOR)/lib/libSFm_fingerprints_service.so
